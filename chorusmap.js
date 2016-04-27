f8339e53c865b821dc364122334ca550d086b571
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2016-04-27 21:21:12 +0800

    adding/removing layers and changing icons ok

--- chorusmap.js
+++ chorusmap.js
@@ -18,7 +18,7 @@ function init(config) {
     G.editor = new JSONEditor($('#config')[0], config);
     G.theMap = L.map('themap').setView([23.5, 120.5], 7);
     G.baseLayer = L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(G.theMap);
-    G.gjLayers = {};
+    G.layerGroups = {};
 
     setTitle();
     reloadSources();
@@ -34,45 +34,79 @@ function setTitle() {
 }
 
 function reloadSources() {
-    // $.getJSON('data/ex1.geojson', addGeojsonLayer);
-    var srcOld = G.editor.getValue().sources;
-    var srcNew = G.editor.getEditor('root.sources').getValue();
-    for (var i=0; i<srcNew.length; ++i) {
-	var sn = srcNew[i];
-	if (! sn.url) { continue; }
-	var found = $.grep(srcOld, function(so) { return sn.url == so.url; });
-	if (found) {
-	    if (found[0].icon==sn.icon && found[0].color==sn.color) {
-		continue;
-	    }
-	    console.log('removing layer ', sn.url);
-	    // G.theMap.removeLayer(G.gjLayers[sn.url]);
-	}
+    // $.getJSON('data/ex1.geojson', addLayerGroup);
+    var srcNew = {};
+    G.editor.getEditor('root.sources').getValue().forEach(function (x) {
+	srcNew[x.url] = x;
+    });
+    var oldURLs = Object.keys(G.layerGroups);
+    var newURLs = Object.keys(srcNew);
+
+    var toRemove = setOps.complement(oldURLs, newURLs);
+    var toAdd = setOps.complement(newURLs, oldURLs);
+    var toChange = setOps.intersection(oldURLs, newURLs).filter(function (x) {
+	return G.layerGroups[x].config.color != srcNew[x].color ||
+	    G.layerGroups[x].config.icon != srcNew[x].icon;
+    });
+console.log('toRemove, toAdd, toChange:');
+console.log(toRemove);
+console.log(toAdd);
+console.log(toChange);
+    toRemove.forEach(function (x) {
+	G.theMap.removeLayer(G.layerGroups[x].LG);
+	delete G.layerGroups[x];
+	// G.theMap.removeLayer(G.layerGroups[sn.url]);
+	printLayers();
+    });
+    toAdd.forEach(function (x) {
 	// https://stackoverflow.com/questions/26699377/how-to-add-additional-argument-to-getjson-callback-for-non-anonymous-function
-        $.getJSON(sn.url, addGeojsonLayer.bind({
-	    'config': sn
+        $.getJSON(x, addLayerGroup.bind({
+	    'config': srcNew[x]
 	}));
+    });
+    toChange.forEach(function (x) {
+	G.layerGroups[x].config = srcNew[x];
+	changeMarker(G.layerGroups[x].LG, G.layerGroups[x].config);
+    });
+}
+
+function addLayerGroup(data) {
+    // http://leafletjs.com/examples/geojson.html
+    console.log('adding layer ' + this.config.url);
+    G.layerGroups[this.config.url] = { 'config': this.config };
+    var marker = L.AwesomeMarkers.icon({
+        'icon': this.config.icon || 'bookmark',
+        'markerColor': this.config.color || 'blue'
+    });
+console.log(marker);
+
+    var st = {
+	pointToLayer: function (f, latlng) {
+	    return L.marker(latlng, { 'icon': marker });
+	}
+    };
+console.log(st);
+
+    G.layerGroups[this.config.url] = {
+	'config': JSON.parse(JSON.stringify(this.config)),
+	'LG': L.geoJson(data, st).addTo(G.theMap)
     }
+    printLayers();
+}
+
+function changeMarker(LG, config) {
+    var marker = L.AwesomeMarkers.icon({
+        'icon': config.icon || 'bookmark',
+        'markerColor': config.color || 'blue'
+    });
+    LG.getLayers().forEach(function (x) {
+	x.setIcon(marker);
+    });
+}
+
+function printLayers() {
     console.log('now we have these layers:');
     G.theMap.eachLayer(function (layer) {
 	console.log(layer);
     });
 }
-
-function addGeojsonLayer(data) {
-    // http://leafletjs.com/examples/geojson.html
-    console.log('loading layer ' + this.config.url);
-    var marker = L.AwesomeMarkers.icon({
-        'icon': this.config.icon,
-        'markerColor': this.config.color
-    });
-
-    var st = {
-	pointToLayer: function (f, latlng) {
-	    return L.marker(latlng, { 'icon':marker });
-	}
-    };
-    G.gjLayers[this.config.url] = L.geoJson(null, st).addTo(G.theMap);
-    G.gjLayers[this.config.url].addData(data);
-}
-
